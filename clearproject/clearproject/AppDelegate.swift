cf084ed056469be8114b63596f3c1c76af93f196
Alexey Khomych <a.khomych@aboutcode.us>
2018-07-10 13:58:03 +0300

    init

---
+++ clearproject/clearproject/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  clearproject
+//
+//  Created by Alexey Khomych on 7/10/18.
+//  Copyright © 2018 Alexey Khomych. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        let window = UIWindow(frame: UIScreen.main.bounds)
+        self.window = window
+        
+        window.rootViewController = ViewController()
+        window.makeKeyAndVisible()
+        
+        return true
+    }
+}
