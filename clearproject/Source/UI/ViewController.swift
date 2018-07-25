5b1ce5eab32e97c11537b5cb9144b06e0cafd365
Alexey Khomych <a.khomych@aboutcode.us>
2018-07-25 12:22:48 +0300

    first example

--- clearproject/Source/UI/ViewController.swift
+++ clearproject/Source/UI/ViewController.swift
@@ -7,29 +7,42 @@
 //
 
 import UIKit
+import SnapKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        self.view.backgroundColor = .lightGray
+        self.createBlueView()
+        self.createGreenView()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func createBlueView() {
+        let blueView = UIView()
+        blueView.backgroundColor = .blue
+        
+        self.view.addSubview(blueView)
+        
+        blueView.translatesAutoresizingMaskIntoConstraints = false
+        blueView.leftAnchor.constraint(equalTo: self.view.leftAnchor).isActive = true
+        blueView.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 50).isActive = true
+        blueView.widthAnchor.constraint(equalTo: self.view.widthAnchor, multiplier: 0.5).isActive = true
+        blueView.heightAnchor.constraint(equalTo: self.view.heightAnchor, multiplier: 0.33).isActive = true
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func createGreenView() {
+        let greenView = UIView()
+        greenView.backgroundColor = .green
+        
+        self.view.addSubview(greenView)
+        
+        greenView.snp.makeConstraints { (make) in
+            make.left.equalToSuperview()
+            make.bottom.equalToSuperview().offset(-50)
+            make.width.equalToSuperview().dividedBy(2)
+            make.height.equalToSuperview().dividedBy(3)
+        }
     }
-    */
-
 }
